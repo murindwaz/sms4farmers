aea0f12f8d02fd1fe5f2d78174dd3c7fb9c2acb7
Pascal Maniraho <murindwaz@yahoo.fr>
2012-06-18 03:15:25 -0400

    JavaScript added sample data to be displayed. There is no CSS to display properly the content

--- js/view.js
+++ js/view.js
@@ -1,20 +1,63 @@
 (function(){
 	
-	window.CustomerTableView = Backbone.View.extend(
+	window.CustomersView = Backbone.View.extend(
 			{
-				template: "#customerTableView",
+				template: "#customersView",
 				events:{},
 				initialize: function(){
 					_.bindAll(this, 'render', 'show');
 					this.collection.fetch();
+				
 					this.collection.bind('all', this.render);
 				},
 				show: function(){}, 
 				render: function(){
 					$(this.el).html( $(this.template).html() );
-					this.collection.each( function(cat) {
-						var template = "";
-						$(this.el).find(".categories").append(template);
+					this.collection.each( function(customer) {
+						var template = "<li><div class='customer-telephone'>"+customer.get('telephone')+"</div></li>";
+						$(this.el).find("#customer-list").append(template);
+					}, this);
+					return this;
+				}
+			});
+
+	window.MessagesView = Backbone.View.extend(
+			{
+				template: "#messagesView",
+				events:{},
+				initialize: function(){
+					_.bindAll(this, 'render', 'show');
+					this.collection.fetch();
+				
+					this.collection.bind('all', this.render);
+				},
+				show: function(){}, 
+				render: function(){
+					$(this.el).html( $(this.template).html() );
+					this.collection.each( function(message) {
+						var template = "<li><div class='message'>"+message.get('message')+"</div></li>";
+						$(this.el).find("#message-list").append(template);
+					}, this);
+					return this;
+				}
+			});
+
+	window.StockView = Backbone.View.extend(
+			{
+				template: "#stockView",
+				events:{},
+				initialize: function(){
+					_.bindAll(this, 'render', 'show');
+					this.collection.fetch();
+				
+					this.collection.bind('all', this.render);
+				},
+				show: function(){}, 
+				render: function(){
+					$(this.el).html( $(this.template).html() );
+					this.collection.each( function(stock) {
+						var template = "<li><div class='stock'>"+stock.get('name')+" @ "+stock.get('price')+"</div></li>";
+						$(this.el).find("#stock-list").append(template);
 					}, this);
 					return this;
 				}
